700f40d5e64ec9b99498da7cfe18ea560277c41e
erwin <winzon353535@gmail.com>
2022-04-13 11:40:16 -0700

    adds index page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import PostIndex from "../views/PostIndex.vue";
 
 const routes = [
   {
@@ -14,6 +15,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/posts",
+    name: "posts",
+    component: PostIndex,
   },
 ];
 
---
+++ src/views/PostIndex.vue
@@ -0,0 +1,15 @@
+<script>
+// import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      message: "I WORK",
+    };
+  },
+};
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
