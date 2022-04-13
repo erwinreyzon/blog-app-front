28d0e69dfce2ed2d226a099ff0cc566dadbdbd02
erwin <winzon353535@gmail.com>
2022-04-13 11:47:08 -0700

    adds data and created to index page

--- src/views/PostIndex.vue
+++ src/views/PostIndex.vue
@@ -1,15 +1,26 @@
 <script>
-// import axios from "axios";
+import axios from "axios";
 
 export default {
   data: function () {
     return {
-      message: "I WORK",
+      message: "Blog Posts",
+      posts: [],
     };
+  },
+  created: function () {
+    axios.get("/posts.json").then((response) => {
+      this.posts = response.data;
+      console.log("Post Index", this.posts);
+    });
   },
 };
 </script>
 
 <template>
   <h1>{{ message }}</h1>
+
+  <div v-for="post in posts" :key="post.id">
+    <p>{{ post.title }}</p>
+  </div>
 </template>
